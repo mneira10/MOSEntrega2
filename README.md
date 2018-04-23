f883361d485aa426786ce1e0b20d4cce37378688
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-04-23 00:26:40 -0500

    documentacion

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MOSEntrega2
+
+Implementación de un algoritmo evolutivo para para el aprendizaje de una red neuronal en python 2. 
+
+El código base fué obtenido de Michael Nielsen en:
+
+https://github.com/mnielsen/neural-networks-and-deep-learning.git
+
+
+
+![image](src/results.png)
--- src/main.py
+++ src/main.py
@@ -3,56 +3,34 @@ import mnist_loader
 import numpy as np
 import dataHandler as dh
 import geneticAlg as ga
+import random
+# from numpy.random import choice
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-# print(len(test_data))
-# net1 = nt.Network([784, 100, 10])
-# net2 = nt.Network([784, 100, 10])
-# print net.biases
-# print "printing evaluation"
-# print net.evaluateNormed(test_data)
-# print net.fitness
-# print "saving network"
-# net.saveNetwork("a")
-# print "network saved"
-# print "loading network"
-# net1 = nt.Network([784, 100, 10])
-# net1.loadNetwork("a")
-# print net.evaluate(test_data)
 
-# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-
-
-# dh.mostrarImagenInd(0)
-# print dh.darDigitoInd(0)
 
 def iterate(networks):
-    
-    # totals, running_total = ga.rankArray(networks)
-    # print totals
-    # print running_total
-    chosen = []
-    #escoger metodo rank 
 
-    # for i in range(20):
-    #     chosen.append(ga.chooseMetodoRanking(totals,running_total))
-    
-    #escogemos los 0.6 mejores
-    
-    for i in range(int(0.6*len(networks))):
+    chosen = []
+    # escogemos a los 10 mejores
+    for i in range(10):
         chosen.append(i)
 
-    print chosen
-    for i in chosen:
-        print networks[i].fitness
+    # for i in chosen:
+    #     print networks[i].fitness
 
     temps = []
     for i in range(len(chosen)/2):
-        son1, son2 = ga.crossover(networks[chosen[i*2]],networks[chosen[i*2+1]])
-        ga.mutate(son1)
-        ga.mutate(son2)
-        ga.mutate(son1)
-        ga.mutate(son2)
+        # se crean hijos segun la mejor poblacion
+        # son1, son2 = ga.crossover(networks[chosen[i*2]],networks[chosen[i*2+1]])
+        # probs= [(len(networks)-x+0.0)/(len(networks)*(len(networks)+1)/2) for x in range(len(networks))]
+        # probs = [1.0/len(networks) for x in range(len(networks))]
+        son1, son2 = ga.crossover(networks[random.choice(chosen)], 
+                                  networks[random.choice(chosen)])
+        for i in range(5):
+            ga.mutate(son1)
+            ga.mutate(son2)
+
         temps.append(son1)
         temps.append(son2)
 
@@ -64,33 +42,32 @@ def iterate(networks):
     # print len(networks)
     # networks = networks.tolist()
 
-    for i in range(int(0.4*len(networks))):
+    while(len(networks) > 10):
         networks.pop(-1)
 
-    for i  in temps:
+    for i in temps:
         networks.append(i)
     # print(len(networks))
-    ga.calcFitnessAll(networks,test_data)
+    ga.calcFitnessAll(networks, test_data)
     prints = []
     for net in networks:
         prints.append(net.fitness)
-    print (prints)
+    print(prints)
     prints = np.array(prints)
     with open("metaResults.txt", "a") as myfile:
-        myfile.write(str(np.average(prints)) + " " + str(np.max(prints))+ " " +str(np.min(prints)) +"\n")
-
+        myfile.write(str(np.average(prints)) + " " +
+                     str(np.max(prints)) + " " + str(np.min(prints)) + "\n")
 
     # for i in networks:
     #     print i.fitness
     # ga.rank(networks)
 
 
-
 networks = ga.genPopulation(10)
 # networks[0].fitness
 # for i in networks:
 #     print i.fitness
-ga.calcFitnessAll(networks,test_data)
+ga.calcFitnessAll(networks, test_data)
 # for i in networks:
 #     print i.fitness
 # ga.rank(networks)
@@ -99,9 +76,6 @@ for i in range(100):
     iterate(networks)
 # for i in networks:
 #     print i.fitness
-
-
-
 
 
 # print np.delete(a, ind2remove)
