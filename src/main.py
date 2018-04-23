4c4140c043563e79275b84b2d649bb187293db5f
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-04-23 00:59:50 -0500

    documentación terminada

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import numpy as np
 import dataHandler as dh
 import geneticAlg as ga
 import random
-# from numpy.random import choice
 
+#se cargan los datos 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 
@@ -16,17 +16,14 @@ def iterate(networks):
     for i in range(10):
         chosen.append(i)
 
-    # for i in chosen:
-    #     print networks[i].fitness
-
     temps = []
     for i in range(len(chosen)/2):
-        # se crean hijos segun la mejor poblacion
-        # son1, son2 = ga.crossover(networks[chosen[i*2]],networks[chosen[i*2+1]])
-        # probs= [(len(networks)-x+0.0)/(len(networks)*(len(networks)+1)/2) for x in range(len(networks))]
-        # probs = [1.0/len(networks) for x in range(len(networks))]
+
+        # se crean hijos segun los mejores individuos de la poblacion
+     
         son1, son2 = ga.crossover(networks[random.choice(chosen)], 
                                   networks[random.choice(chosen)])
+        #y se mutan
         for i in range(5):
             ga.mutate(son1)
             ga.mutate(son2)
@@ -34,50 +31,38 @@ def iterate(networks):
         temps.append(son1)
         temps.append(son2)
 
-    # networks = np.array(networks)
-    # uniq = list(set(chosen))
-    # networks = np.delete(networks,uniq)
-    # networks = networks[chosen]
-    # print uniq
-    # print len(networks)
-    # networks = networks.tolist()
-
+    #se eliminan todos los individuos salvo los 10 mejores
     while(len(networks) > 10):
         networks.pop(-1)
 
+    #se agregan los hijos
     for i in temps:
         networks.append(i)
-    # print(len(networks))
+
+    #se calcula el fitness de todos y se ordena la poblacion
+    #por fitness descendientemente
     ga.calcFitnessAll(networks, test_data)
+
+    #imprimimos a consola 
     prints = []
     for net in networks:
         prints.append(net.fitness)
     print(prints)
+
+    #exportamos a un archivo al mejor de esta generacion, 
+    #el promedio de la generacion y el peor de la generacion
     prints = np.array(prints)
     with open("metaResults.txt", "a") as myfile:
         myfile.write(str(np.average(prints)) + " " +
                      str(np.max(prints)) + " " + str(np.min(prints)) + "\n")
 
-    # for i in networks:
-    #     print i.fitness
-    # ga.rank(networks)
 
-
+#se inicializa la poblacion
 networks = ga.genPopulation(10)
-# networks[0].fitness
-# for i in networks:
-#     print i.fitness
-ga.calcFitnessAll(networks, test_data)
-# for i in networks:
-#     print i.fitness
-# ga.rank(networks)
 
+#se calcula el fitness inicial
+ga.calcFitnessAll(networks, test_data)
+
+#se realizan 100 generaciones
 for i in range(100):
     iterate(networks)
-# for i in networks:
-#     print i.fitness
-
-
-# print np.delete(a, ind2remove)
-
-# son1 ,son2 = ga.crossover(net1,net2)
